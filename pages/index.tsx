c88e5420f02f2e8c054233ae4e82300f98fcf3db
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-12-10 14:00:34 +0300

    feature/QUERY: added settings for json server and react-query request

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import { Inter } from 'next/font/google'
 import { useQuery } from 'react-query'
+import { CountryService, ICountry } from '@/app/services/country.service'
 import styles from '@/styles/Home.module.css'
-import { CountryService } from '@/app/services/country.service'
+import { useState } from 'react'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { isLoading, data: response, error, status } = useQuery(
+const [countries, setCountries] = useState<ICountry[]>([])
+
+  const { isLoading, data, error } = useQuery(
     'country list',
-    () => CountryService.getAll(),
-  )
+    () => CountryService.getAll(), {
+      onSuccess: ({ data }: any) => {
+        setCountries(data)
+      },
+      onError: (error: any) => {
+        alert(error.message)
+      }
+    })
 
   return (
     <div className={styles.container}>
@@ -18,9 +28,9 @@ export default function Home() {
         {error && <div style={{ color: 'red' }}>{error.message}</div>}
         {isLoading ? (
           <div>Loading...</div>
-        ) : response?.data.length ? (
+        ) : countries.length ? (
           <div className={styles.grid}>
-            {response.data.map((country: any) => (
+            {countries.map((country: any) => (
               <div className={styles.card} key={country.id}>
                 <Image
                   alt={country.title}
